0e9759e99e04cdb645134f121fedb14b305e3f05
MarkusFuchs68 <markus_fuchs@gmx.net>
2025-05-15 11:07:51 +0200

    fixed pytest test discovery

--- services/ml_train_hub/tests/conftest.py
+++ services/ml_train_hub/tests/conftest.py
@@ -1,10 +1,10 @@
 import pytest
-from app.main import app
 from fastapi.testclient import TestClient
+from ml_train_hub.app.main import app
 
 
 # fixture for TestClient instance
 @pytest.fixture(scope="module")
-def test_host_backend_client():
+def test_train_hub_client():
     client = TestClient(app)
     yield client
