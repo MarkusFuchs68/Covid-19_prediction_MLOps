beaef485cc42008edebf17d42dd31ae8f30d4d0c
MarkusFuchs68 <markus_fuchs@gmx.net>
2025-05-15 10:59:27 +0200

    rearranged test paths

--- services/ml_train_hub/app/mlflow_svc.py
+++ services/ml_train_hub/app/mlflow_svc.py
@@ -7,19 +7,20 @@ from mlflow import MlflowClient
 # connect to our mlflow server
 mlflow_client = MlflowClient(tracking_uri="http://127.0.0.1:8081")
 
-# set our experiment project name
-experiment_name = "Covid_Models"
-covid_experiment = mlflow.set_experiment(experiment_name)
-
 # other variable definitions
 artifact_path = "covid_cnn"  # the path to the models in mlflow
 
 
 # model experiment tracking
 def log_mlflow_experiment(
-    params, metrics, model, register_model=False, model_name="Covid-19"
+    params,
+    metrics,
+    model,
+    experiment_name="Covid_Models",
+    register_model=False,
+    model_name="Covid-19",
 ):
-
+    mlflow.set_experiment(experiment_name)
     run_name = "run_" + datetime.now().strftime("%Y%m%d_%H%M%S")
 
     with mlflow.start_run(run_name=run_name):
@@ -27,13 +28,12 @@ def log_mlflow_experiment(
         mlflow.log_metrics(metrics)
         if register_model:  # log experiment and register its model
             modelinfo = mlflow.tensorflow.log_model(
-                model=model,
-                artifact_path=artifact_path,
-                registered_model_name=model_name,
+                model=model, artifact_path=None, registered_model_name=model_name
             )
         else:  # just log the experiment
             modelinfo = mlflow.tensorflow.log_model(
-                model=model, artifact_path=artifact_path
+                model=model,
+                artifact_path=None,
             )
         return modelinfo
 
