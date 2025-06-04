8d8f5d62a974d1a921a922b60e67cd6dd810339d
markus <markus_fuchs@gmx.net>
2025-06-04 18:33:41 +0200

    fixed pre-commit

--- services/ml_auth/app/exceptions/auth_exceptions.py
+++ services/ml_auth/app/exceptions/auth_exceptions.py
@@ -2,6 +2,7 @@ class InvalidArgumentException(Exception):
     """
     Invalid argument exception
     """
+
     def __init__(self, message="Params are not correct or empty."):
         self.message = message
         super().__init__(self.message)
@@ -11,6 +12,7 @@ class FailedAuthentification(Exception):
     """
     Wrong credentials or unknown user
     """
+
     def __init__(self, message="Wrong credentials or unknown user."):
         self.message = message
         super().__init__(self.message)
